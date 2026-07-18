2fe9fab5cb6d265c10545c6b278dcc9d875d4afa
Steven Chow <234322+ssc@users.noreply.github.com>
2026-07-18 12:35:33 -0700

    rollback: set public site to v1.0.99

--- phoneui/rs-phoneui-shared.js
+++ phoneui/rs-phoneui-shared.js
@@ -16,7 +16,7 @@
  *   .getCaptureStream(video) → MediaStream | null   (wraps video.captureStream or canvas fallback)
  *   .stopCaptureStream()     → void
  */
-window.RS_JS_BUILD_TIME = '2026-07-16 01:18 UTC';
+window.RS_JS_BUILD_TIME = '2026-07-18 19:35 UTC';
 
 window.RsDevGenerators = (function () {
     'use strict';
